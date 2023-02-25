d70a8c3f5bd03a1fd7a407ef4dde5297691f612a
Sergio <aguilatroll1081@gmail.com>
2023-02-25 20:10:08 +0100

    crud de videojuegos

--- AllKeys/AdminGames.xaml.cs
+++ AllKeys/AdminGames.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AllKeys.Modelo;
+using ExamenVentas.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Usuarios.Modelo;
 
 namespace AllKeys
 {
@@ -20,9 +23,72 @@ namespace AllKeys
     /// </summary>
     public partial class AdminGames : Page
     {
+        Videojuego videojuego=new Videojuego();
+        UnitOfWork bd= new UnitOfWork();
+        Boolean nuevo = true;
         public AdminGames()
         {
             InitializeComponent();
+            gbFormularioV.DataContext = videojuego;
+            dgVideojuegos.ItemsSource=bd.VideojuegosRepository.GetAll();
+            dgVideojuegos.SelectedIndex=-1;
+        }
+
+        private void dgVideojuegos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgVideojuegos.SelectedIndex != -1)
+            {
+                videojuego = (Videojuego)dgVideojuegos.SelectedItem;
+                gbFormularioV.DataContext = videojuego;
+                nuevo = false;
+            }
+        }
+        private void Limpiar()
+        {
+            videojuego = new Videojuego();
+            gbFormularioV.DataContext = videojuego;
+            nuevo = true;
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            String errores = Validacion.errores(videojuego);
+            if (errores.Equals(""))
+            {
+                if (nuevo)
+                {
+                    bd.VideojuegosRepository.Añadir(videojuego);
+                    bd.Save();
+                    dgVideojuegos.ItemsSource = bd.VideojuegosRepository.GetAll();
+                }
+                else
+                {
+                    bd.VideojuegosRepository.Update(videojuego);
+                    bd.Save();
+                }
+
+                Limpiar();
+            }
+            else
+            {
+                //LibreriaV7.MostrarSnackbar(sbNotification, errores, letra: "#FFFF0000");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgVideojuegos.SelectedIndex != -1)
+            {
+                bd.VideojuegosRepository.Delete(videojuego);
+                bd.Save();
+                Limpiar();
+                dgVideojuegos.ItemsSource = bd.VideojuegosRepository.GetAll();
+            }
         }
     }
 }
--- AllKeys/DAL/VideojuegosRepository.cs
+++ AllKeys/DAL/VideojuegosRepository.cs
@@ -16,6 +16,7 @@ namespace AllKeys.DAL
         }
         public Videojuego videojuegoCompleto(int videojuegoId)
         {
+            // includeProperties  se está solicitando que las ventas asociadas a ese cliente también se carguen en la memoria.
             return Get(v => v.VideojuegoId == videojuegoId, includeProperties: "Copias").FirstOrDefault();
         }
     }
