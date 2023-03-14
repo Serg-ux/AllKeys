ba968d4186cc48dade95aa5100265a55f4477183
Serg-ux <aguilatroll1081@gmail.com>
2023-03-14 14:41:12 +0100

    arreglo de que se muestren todas las copias

--- AllKeys/AdminGame.xaml.cs
+++ AllKeys/AdminGame.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using AllKeys.DAL;
 using AllKeys.Modelo;
 using ExamenVentas.DAL;
 using System;
@@ -144,7 +145,7 @@ namespace AllKeys
                     bd.Save();
                 }
                 Limpiar_Copia();
-                dgCopias.ItemsSource = bd.CopiasRepository.GetAll();
+                dgCopias.ItemsSource= bd.CopiasRepository.CopiasFiltro(videojuego.VideojuegoId);
                 MessageBox.Show("Guardado correctamente", "Informacion", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else MessageBox.Show(errores, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -155,7 +156,7 @@ namespace AllKeys
             copia = new Copia();
             tbCodCopia.Text = copia.CopiaCod;
             new_copia = true;
-            dgCopias.ItemsSource = bd.CopiasRepository.GetAll();
+            dgCopias.ItemsSource = bd.CopiasRepository.CopiasFiltro(videojuego.VideojuegoId);
         }
 
         private void btnBorrarCopia_Click(object sender, RoutedEventArgs e)
@@ -170,7 +171,7 @@ namespace AllKeys
                     bd.CopiasRepository.Delete(copia);
                     bd.Save();
                     Limpiar_Copia();
-                    dgCopias.ItemsSource = bd.CopiasRepository.GetAll();
+                    dgCopias.ItemsSource = bd.CopiasRepository.CopiasFiltro(videojuego.VideojuegoId);
 
                 }
 
